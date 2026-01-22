1a844b345d5a2cf707d65c053a0c2790a5f2b0b2
eagle <lefttolive@proton.me>
2026-01-22 17:05:39 -0300

    format

--- mqtt-auth-biscuit/crates/mosquitto-plugin/src/auth.rs
+++ mqtt-auth-biscuit/crates/mosquitto-plugin/src/auth.rs
@@ -1,7 +1,7 @@
-#[cfg(not(kani))]
-use crate::jwt_handler::{verify_jwt_token, Claims};
 #[cfg(kani)]
 use crate::jwt_handler::Claims;
+#[cfg(not(kani))]
+use crate::jwt_handler::{verify_jwt_token, Claims};
 #[cfg(not(kani))]
 use jsonwebtoken::errors::ErrorKind;
 use jsonwebtoken::DecodingKey;
